b96c1e22368f3410650ee7a625fb597d39919ecc
carmemias <info@carmemias.com>
2019-03-02 22:46:13 +0000

    Started SweepstakeDetails component

---
+++ sweep_mate_client/src/components/SweepstakeDetails.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+	<div id="sweep-details">
+		<h1>{{ sweep.title }}</h1>
+		<p>{{ sweep.cutOffDate }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "sweepstake-details",
+	props: ['sweep']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- sweep_mate_client/src/views/SingleSweepstake.vue
+++ sweep_mate_client/src/views/SingleSweepstake.vue
@@ -1,16 +1,17 @@
 <template lang="html">
-
-  <!-- <sweepstake-details/> -->
-  <new-player-form v-if="!sweepstakeClosed()" :sweep="sweep">
-  </new-player-form>
-	<p class="notification" v-else>This sweepstake is no longer available.</p>
-  <!-- <sweepstake-results v-else="showResult"></sweepstake-results> -->
-
+	<div class="single-sweep">
+		<sweepstake-details :sweep="sweep"/>
+		<new-player-form v-if="!sweepstakeClosed()" :sweep="sweep">
+		</new-player-form>
+		<p class="notification" v-else>This sweepstake is no longer available.</p>
+		<!-- <sweepstake-results v-else="showResult"></sweepstake-results> -->
+	</div>
 </template>
 
 <script>
 import { eventBus } from '../main.js';
 import NewPlayerForm from '../components/NewPlayerForm.vue';
+import SweepstakeDetails from '../components/SweepstakeDetails.vue';
 
 export default {
 	data(){
@@ -21,21 +22,22 @@ export default {
 	mounted(){
 		const id = this.$route.params.id
 		fetch("http://localhost:3000/api/sweepstakes/" + id)
-			.then(res => res.json())
-			.then(res => this.sweep = res)
+		.then(res => res.json())
+		.then(res => this.sweep = res)
 
-			eventBus.$on('option-allocated', optionToRemove => this.makeOptionUnavailable(optionToRemove));
+		eventBus.$on('option-allocated', optionToRemove => this.makeOptionUnavailable(optionToRemove));
 	},
 	components: {
-		NewPlayerForm
+		NewPlayerForm,
+		SweepstakeDetails
 	},
-  methods: {
-    sweepstakeClosed() {
+	methods: {
+		sweepstakeClosed() {
 			const today = new Date();
 			const cutOffDate = new Date(this.sweep.cutOffDate);
-      //returns true if sweepstake cut off date is past
-      return today >= cutOffDate;
-    },
+			//returns true if sweepstake cut off date is past
+			return today >= cutOffDate;
+		},
 		makeOptionUnavailable(optionName){
 			const optionToRemove = this.sweep.options.find( option => option.name === optionName);
 			optionToRemove.allocated = true
@@ -47,10 +49,10 @@ export default {
 				headers: { 'Content-Type': 'application/json'}
 			})
 		},
-    showResult() {
-      //return winners name and option
-    }
-  }
+		showResult() {
+			//return winners name and option
+		}
+	}
 }
 </script>
 
