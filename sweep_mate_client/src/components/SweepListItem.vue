66e485255def2cdb354bbe1a3677b94937f339e0
Neil Watkins <neil.watkins1@sky.com>
2019-03-02 16:20:16 +0000

    started sweeplistitem

--- sweep_mate_client/src/components/SweepListItem.vue
+++ sweep_mate_client/src/components/SweepListItem.vue
@@ -1,10 +1,31 @@
 <template lang="html">
-<h1></h1>
+  <div class="sweep-list-item" :style="style">
+    <h3>{{sweep.title}}</h3>
+    <router-link :to="{name: 'admin-edit-sweepstake', params: {id: this.sweep._id}}" class="button">Edit</router-link>
+  </div>
 </template>
 
 <script>
 export default {
 }
+
+
+export default {
+
+  },
+  data() {
+
+    }
+  },
+  mounted(){
+    fetch("http://localhost:3000/api/sweepstakes/")
+    .then(res => res.json())
+    .then(data => this.sweeps = data)
+  }
+}
+
+
+
 </script>
 
 <style lang="css" scoped>
