11c0a877e1241582092905a165fc8ec14ee33d80
carmemias <info@carmemias.com>
2019-03-03 06:44:32 +0000

    Added picture to Sweepstake details

--- sweep_mate_client/src/components/SweepstakeDetails.vue
+++ sweep_mate_client/src/components/SweepstakeDetails.vue
@@ -1,14 +1,26 @@
 <template lang="html">
 	<div id="sweep-details">
 		<h1>{{ sweep.title }}</h1>
-		<p>{{ sweep.cutOffDate }}</p>
+		<p>Available until: {{ cutOffDate }}</p>
+		<img :src="sweep.picture" alt="sweep.title" />
 	</div>
 </template>
 
 <script>
 export default {
 	name: "sweepstake-details",
-	props: ['sweep']
+	props: ['sweep'],
+	computed: {
+		cutOffDate: function(){
+			const cutOffDate = new Date(this.sweep.cutOffDate)
+			return cutOffDate.toLocaleDateString('en-GB', {
+				weekday: 'long',
+				year: 'numeric',
+				month: 'long',
+				day: 'numeric'
+			});
+		}
+	}
 }
 </script>
 
