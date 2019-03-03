7c52f26befdb5ddd9458af74149257cd2cb62e96
Neil Watkins <neil.watkins1@sky.com>
2019-03-03 12:22:11 +0000

    drafted ListSweepstakes.vue

---
+++ sweep_mate_client/src/components/ListSweepstakes.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <single-sweeps :sweepsList="sweepsList"></single-sweeps>
+</template>
+
+<script>
+import SweepListItem from './SweepListItem.vue'
+export default {
+  // props: [''],
+  data(){
+    return {
+      sweepsList: []
+    }
+  },
+  components: {
+    SweepListItem
+  },
+  mounted() {
+    fetch("http://localhost:3000/api/sweepstakes/")
+    .then(res => res.json())
+    .then(sweepstakes => this.sweepsList = sweeps)
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- sweep_mate_client/src/components/SweepListItem.vue
+++ sweep_mate_client/src/components/SweepListItem.vue
@@ -3,72 +3,21 @@
     <h3>{{sweep.title}}</h3>
     <router-link :to="{name: 'admin-edit-sweepstake', params: {id: this.sweep._id}}" class="button">Edit</router-link>
     <button type="button" name="delete-sweep" v-on:click="">Delete</button>
+    <button type="button" name="publish-sweep" v-on:click="">Publish</button>
   </div>
 </template>
 
 <script>
+import ListSweepstakes from './ListSweepstakes.vue'
 export default {
+  name: "SingleSweeps",
+  props: ['sweepsList']
 }
 
-
-export default {
-
-  },
-  data() {
-
-    }
-  },
-  mounted(){
-    fetch("http://localhost:3000/api/sweepstakes/")
-    .then(res => res.json())
-    .then(data => this.sweeps = data)
-  }
-}
 
 
 
 </script>
 
 <style lang="css" scoped>
-</style>
-
-
-
-
-
-
-
-
-
-<template lang="html">
-  <div class="">
-    <div class="single-player" v-for="(player, index) in playersList" index="id">
-      <p>Player Name: {{player.name}}</p>
-      <div class="" v-for="game in player.games">
-        <p>Allocated: {{game.allocatedOption}}</p>
-      </div>
-    </div>
-  </div>
-
-</template>
-
-<script>
-import ListPlayers from './ListPlayers.vue'
-export default {
-  name: "SinglePlayers",
-  props: ['playersList']
-}
-</script>
-
-<style lang="css" scoped>
-
-.single-player {
-  display: flex;
-  border-style: solid;
-  justify-content: center;
-  margin-left: auto;
-  margin-right: auto;
-  justify-content: space-around;
-  margin: 2px;
-}
 </style>
