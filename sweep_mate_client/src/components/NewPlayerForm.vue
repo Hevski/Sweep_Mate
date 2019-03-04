43582f65b42d93125b0a6667846c8a8ab128464a
Heather <heathercdavies@gmail.com>
2019-03-04 15:46:51 +0000

    set message to appear when sweep is full

--- sweep_mate_client/src/components/NewPlayerForm.vue
+++ sweep_mate_client/src/components/NewPlayerForm.vue
@@ -33,6 +33,7 @@ export default {
 				games: []
 			},
 			yourOption: ''
+
 			// createdPlayer: null
 		}
 	},
--- sweep_mate_client/src/views/SingleSweepstake.vue
+++ sweep_mate_client/src/views/SingleSweepstake.vue
@@ -1,9 +1,10 @@
 <template lang="html">
   <div v-if="sweep">
-		<sweepstake-details :sweep="sweep" :sweepstakeClosed="sweepstakeClosed"/>
-		<list-players :playersList="playersList" :sweepsPlayers="sweepsPlayers"></list-players>
-    <new-player-form v-if="!sweepstakeClosed()" :sweep="sweep"></new-player-form>
-		<sweepstake-results :sweepsPlayers="sweepsPlayers" v-else="showResult"></sweepstake-results>
+    <sweepstake-details :sweep="sweep" :sweepstakeClosed="sweepstakeClosed"/>
+    <p>{{message}}</p>
+    <list-players :playersList="playersList" :sweepsPlayers="sweepsPlayers"></list-players>
+    <new-player-form v-if="!sweepstakeClosed() && optionsLength > 0" :sweep="sweep"></new-player-form>
+    <sweepstake-results :sweepsPlayers="sweepsPlayers" v-else="showResult"></sweepstake-results>
   </div>
 </template>
 
@@ -15,59 +16,62 @@ import ListPlayers from '../components/ListPlayers.vue';
 import sweepstakeResults from '../components/SweepstakeResults.vue';
 
 export default {
-	data(){
-		return {
-			sweep: '',
+  data(){
+    return {
+      sweep: '',
       playersList: [],
-      sweepsPlayers: []
-		}
-	},
-	mounted(){
-		// get the sweepstake object
-		const id = this.$route.params.id
-		fetch("http://localhost:3000/api/sweepstakes/" + id)
-		.then(res => res.json())
-		.then(res => {
-			this.sweep = res;
-
-			eventBus.$on('option-allocated', allocatedOption => this.makeOptionUnavailable(allocatedOption));
-		})
+      sweepsPlayers: [],
+      message: '',
+      optionsLength: 0
+    }
+  },
+  mounted(){
+    // get the sweepstake object
+    const id = this.$route.params.id
+    fetch("http://localhost:3000/api/sweepstakes/" + id)
+    .then(res => res.json())
+    .then(res => {
+      this.sweep = res
+      this.optionsLength = this.sweep.options.filter(option => option.allocated === false).length
+      eventBus.$on('option-allocated', allocatedOption => {this.makeOptionUnavailable(allocatedOption)
+      this.reduceOptions()
+     })
+    });
     fetch("http://localhost:3000/api/players/")
     .then(res => res.json())
     .then(players => {
       this.playersList = players
       this.sweepsPlayers = this.filterSweepPlayers()
     });
-
-	},
-	components: {
-		SweepstakeDetails,
-		ListPlayers,
-		NewPlayerForm,
+  },
+  components: {
+    SweepstakeDetails,
+    ListPlayers,
+    NewPlayerForm,
     sweepstakeResults
-	},
-	methods: {
-		sweepstakeClosed() {
-			const today = new Date();
-			const cutOffDate = this.sweep.cutOffDate ? new Date(this.sweep.cutOffDate) : null ;
+  },
+  methods: {
+    sweepstakeClosed() {
+      const today = new Date();
+      const cutOffDate = this.sweep.cutOffDate ? new Date(this.sweep.cutOffDate) : null ;
 
-			//returns true if sweepstake cut off date is past
-			return today >= cutOffDate;
-		},
-		makeOptionUnavailable(optionName){
-			const optionToRemove = this.sweep.options.find( option => option.name === optionName);
-			optionToRemove.allocated = true
+      //returns true if sweepstake cut off date is past
+      return today >= cutOffDate;
+    },
+    makeOptionUnavailable(optionName){
+      const optionToRemove = this.sweep.options.find( option => option.name === optionName);
+      optionToRemove.allocated = true
 
-			//save changes to database
-			fetch("http://localhost:3000/api/sweepstakes/" + this.sweep._id, {
-				method: 'put',
-				body: JSON.stringify(this.sweep),
-				headers: { 'Content-Type': 'application/json'}
-			})
-		},
-		showResult() {
-			//.find player check that allocated option === final answer
-		},
+      //save changes to database
+      fetch("http://localhost:3000/api/sweepstakes/" + this.sweep._id, {
+        method: 'put',
+        body: JSON.stringify(this.sweep),
+        headers: { 'Content-Type': 'application/json'}
+      })
+    },
+    showResult() {
+      //.find player check that allocated option === final answer
+    },
     filterSweepPlayers(){
       const finalResult = []
       this.playersList.forEach((player) => {
@@ -77,8 +81,14 @@ export default {
         }
       })
       return finalResult
+    },
+    reduceOptions(){
+      this.optionsLength -= 1
+      if (this.optionsLength < 1) {
+      this.message = "Sweep full"
+     }
     }
-	}
+  }
 }
 </script>
 
