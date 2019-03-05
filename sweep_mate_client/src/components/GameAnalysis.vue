793a86c677abc6641673fa76285eb1023a15d698
Heather <heathercdavies@gmail.com>
2019-03-05 10:55:04 +0000

    added chart set up

---
+++ sweep_mate_client/src/components/GameAnalysis.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+
+</template>
+
+<script>
+import { GChart } from 'vue-google-charts'
+export default {
+  components: {
+    GChart
+  },
+  data () {
+    return {
+      chartData: [
+      ],
+      chartOptions: {
+        chart: {
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
