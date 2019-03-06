a639681caafb35c3a32cccfb8347318fb932f49d
carmemias <info@carmemias.com>
2019-03-06 07:02:36 +0000

    Added email exists check

--- sweep_mate_client/src/views/AdminHome.vue
+++ sweep_mate_client/src/views/AdminHome.vue
@@ -1,14 +1,14 @@
 <template lang="html">
-  <div class="admin-home">
-    <section>
-      <h1>Your Games</h1>
-    </section>
-    <section>
-      <new-sweep-button></new-sweep-button>
-    </section>
-    <list-sweepstakes :sweepsList="sweepsList"></list-sweepstakes>
-    <game-analysis :sweepsList="sweepsList"></game-analysis>
-  </div>
+	<div class="admin-home">
+		<section>
+			<h1>Your Games</h1>
+		</section>
+		<section>
+			<new-sweep-button></new-sweep-button>
+		</section>
+		<list-sweepstakes :sweepsList="sweepsList"></list-sweepstakes>
+		<game-analysis :sweepsList="sweepsList"></game-analysis>
+	</div>
 
 </template>
 
@@ -18,25 +18,25 @@ import NewSweepButton from '../components/NewSweepButton'
 import GameAnalysis from '../components/GameAnalysis'
 import {eventBus} from '../main.js'
 export default {
-  data (){
-    return {
-      sweepsList: []
-    }
-  },
-  components: {
-    ListSweepstakes,
-    NewSweepButton,
-    GameAnalysis
-  },
-  mounted() {
-    fetch("http://localhost:3000/api/sweepstakes/")
-    .then(res => res.json())
-    .then(data => {this.sweepsList = data
-    eventBus.$on("sweep-deleted", (id) => {
-      this.sweepsList = this.sweepsList.filter(sweep => sweep._id !== id)
-    })
-  })
- }
+	data (){
+		return {
+			sweepsList: []
+		}
+	},
+	components: {
+		ListSweepstakes,
+		NewSweepButton,
+		GameAnalysis
+	},
+	mounted() {
+		fetch("http://localhost:3000/api/sweepstakes/")
+		.then(res => res.json())
+		.then(data => {this.sweepsList = data
+			eventBus.$on("sweep-deleted", (id) => {
+				this.sweepsList = this.sweepsList.filter(sweep => sweep._id !== id)
+			})
+		})
+	}
 }
 </script>
 
